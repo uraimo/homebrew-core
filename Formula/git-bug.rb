03b301fbc58ee079e5d0afb69913ef5f2503e067
Alexander Bayandin <a.bayandin@gmail.com>
2020-04-23 13:02:51 +0100

    Update git-bug.rb

--- Formula/git-bug.rb
+++ Formula/git-bug.rb
@@ -1,34 +1,39 @@
 class GitBug < Formula
   desc "Distributed, offline-first bug tracker embedded in git, with bridges"
   homepage "https://github.com/MichaelMure/git-bug"
-  url "https://github.com/MichaelMure/git-bug/archive/0.7.1.tar.gz"
-  sha256 "78a6c7dee159cdad4ad69066d6d8fc4b7c259d5ea6351aaf6709b6ac03ff3d2f"
+  url "https://github.com/MichaelMure/git-bug.git",
+    :tag      => "0.7.1",
+    :revision => "2d64b85db71a17ff3277bbbf7ac9d8e81f8e416c"
+  head "https://github.com/MichaelMure/git-bug.git"
 
   depends_on "go" => :build
 
   def install
-    system "make", "build"
-    bin.install "git-bug"
+    ENV["GOBIN"] = bin
+    system "make", "install"
+
+    man1.install Dir["doc/man/*.1"]
+    doc.install Dir["doc/md/*.md"]
+    bash_completion.install "misc/bash_completion/git-bug"
+    zsh_completion.install "misc/zsh_completion/git-bug" => "_git-bug"
   end
 
   test do
     # Version
-    assert_includes shell_output("#{bin}/git-bug --version"), "git-bug version"
+    assert_match version.to_s, shell_output("#{bin}/git-bug --version")
     # Version through git
-    assert_includes shell_output("git bug --version"), "git-bug version"
+    assert_match version.to_s, shell_output("git bug --version")
 
-    output = ""
     mkdir testpath/"git-repo" do
       system "git", "init"
       system "git", "config", "user.name", "homebrew"
       system "git", "config", "user.email", "a@a.com"
       system "yes 'a b http://www/www' | git bug user create"
-      system "git", "bug", "add", "-t", "\"Issue 1\"", "-m", "\"Issue body\""
-      system "git", "bug", "add", "-t", "\"Issue 2\"", "-m", "\"Issue body\""
-      system "git", "bug", "add", "-t", "\"Issue 3\"", "-m", "\"Issue body\""
-      output = shell_output("git bug ls")
-    end
+      system "git", "bug", "add", "-t", "Issue 1", "-m", "Issue body"
+      system "git", "bug", "add", "-t", "Issue 2", "-m", "Issue body"
+      system "git", "bug", "add", "-t", "Issue 3", "-m", "Issue body"
 
-    assert_includes output, "Issue 2"
+      assert_match "Issue 2", shell_output("git bug ls")
+    end
   end
 end
